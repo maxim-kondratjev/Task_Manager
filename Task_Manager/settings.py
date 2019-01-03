45031f66ed93c4e45e03df7b1fe0ba67502ec7fb
maximusman533 <192118@mail.ru>
2019-01-04 00:18:08 +0300

    Static

--- Task_Manager/settings.py
+++ Task_Manager/settings.py
@@ -14,7 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "/TM/static"),
+    os.path.join(BASE_DIR, "/TM/static/js"),
+    os.path.join(BASE_DIR, "/TM/static/css"),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
