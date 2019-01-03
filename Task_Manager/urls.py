3c045c4632611d960751f4787223461c77901997
maximusman533 <192118@mail.ru>
2019-01-04 00:33:19 +0300

    Static

--- Task_Manager/urls.py
+++ Task_Manager/urls.py
@@ -36,3 +36,5 @@ urlpatterns = [
     path('<int:id>/', include(('TM.task_urls', 'TM'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
